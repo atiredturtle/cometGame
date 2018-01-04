30a71b9121b5775f6ce8b2a7d4f4ecb96e291d66
atiredturtle <guy@segev.com>
2018-01-04 07:17:08 -0500

    remove bullets and move parts to different files

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+import { Enemy }     from "Enemy";
+import { Character } from "Character";
+import { Explosion } from "Explosion";
+
 var canvas = document.getElementById('mycanvas');
 var ctx = canvas.getContext('2d');
 const CELLSIZE = 10;
@@ -10,7 +14,6 @@ const UP_KEY = 38;
 const DOWN_KEY = 40; 
 const LEFT_KEY = 37;
 const RIGHT_KEY = 39; 
-
 const SPACE_KEY = 32;
 
 // DIRECTIONS
@@ -21,24 +24,22 @@ var DOWN = false;
 
 const MIN_ENEMY_SPAWN_TIME = 10
 const START_ENEMY_SPAWN_TIME = MIN_ENEMY_SPAWN_TIME + 60
+
+// GLOBALS
 var ENEMY_SPAWN_TIME = undefined;
 var timeSinceLastEnemy = 0;
 var bullets = [];
 var enemies = [];
-var mouse = {
-    clicked: false,
-    x: undefined,
-    y: undefined
-}
 var score = 0;
 var time = undefined;
+
 // Redefine common Math function
 var abs  = (n)=>Math.abs(n);
 var sqrt = (n)=>Math.sqrt(n);
 var sin  = (n)=>Math.sin(n);
 var cos  = (n)=>Math.cos(n);
 
-// common functinos
+// common functions
 var min = (x,y)=>(x < y ? x : y)
 var max = (x,y)=>(x > y ? x : y)
 var rgba = (r,g,b,a=1)=>(`rgba(${r},${g},${b},${a})`);
@@ -90,222 +91,6 @@ var displayText = (text,x,y,fontSize) => {
     ctx.fillText(text, x, y);
 }
 
-var Bullet = function(x, y, toX, toY) {
-    this.width = CELLSIZE/3;
-    this.height = this.width;
-    this.color = "white";
-    this.speed = 30;
-    this.x = x;
-    this.y = y;
-    this.getDirection = () => {
-        let width = toX - x;
-        let height = toY - y;
-        let theta = Math.atan(abs(height/width));
-        this.vx = width/abs(width) * this.speed * cos(theta);
-        this.vy = height/abs(height) * this.speed * sin(theta);
-    }
-    this.getDirection();
-    bullets.push(this);
-    this.update = () => {
-        this.centreX = this.x - this.width/2;
-        this.centreY = this.y - this.height/2;
-        this.x += this.vx;
-        this.y += this.vy;
-        // for SQUARE
-        // ctx.fillStyle = this.color;
-        // ctx.fillRect(this.centreX, this.centreY, this.width, this.height); 
-        
-        // FOR CIRCLE
-        drawCircle(this.x,this.y, this.width, this.color)
-    }
-}
-
-var Enemy = function(){
-    this.speed = 2;
-    this.color = ["white"]//["red", "orange", "yellow"]
-    this.health = this.color.length - 1;
-    this.width = CELLSIZE;
-    this.height = CELLSIZE;
-    this.getStart = ()=>{
-        let quadrant = parseInt(Math.random()*4);
-        let randWidth = Math.random()*canvas.width;
-        let randHeight = Math.random()*canvas.height;
-        switch (quadrant){
-            // left
-            case 0:
-                this.x = -this.width;
-                this.y = randHeight;
-                // console.log(`left ${this.x}, ${this.y}`);
-                break;
-            // right
-            case 1:
-                this.x = canvas.width + this.width;
-                this.y = randHeight;
-                // console.log(`right ${this.x}, ${this.y}`);
-                break;
-            //up
-            case 2: 
-                this.x = randWidth;
-                this.y = -this.height;
-                // console.log(`up ${this.x}, ${this.y}`);
-                break;
-            // down
-            case 3:
-                this.x = randWidth;
-                this.y = canvas.height + this.height;
-                // console.log(`down ${this.x}, ${this.y}`);
-                break;
-        }
-    }
-    this.getDirection = ()=>{
-        let width = char.x - this.x;
-        let height = char.y - this.y;
-        let theta = Math.atan(abs(height/width));
-        this.vx = width/abs(width) * this.speed * cos(theta);
-        this.vy = height/abs(height) * this.speed * sin(theta);
-    }
-    this.update = ()=>{
-        ctx.fillStyle = this.color[this.health];
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-        this.getDirection();
-        this.x += this.vx;
-        this.y += this.vy;
-    }
-    this.getStart();
-}
-
-var Explosion = function(obj, dist, maxSize, color){
-    this.maxSize = maxSize;
-    this.size = CELLSIZE;
-    this.growRate = 1.5;
-    this.done = false
-    this.color = color;
-    this.vx = 0;
-    this.vy = 0;
-    this.update = function(){
-        if (this.size < this.maxSize){
-            this.size *= this.growRate;
-            drawCircle(obj.x, obj.y, this.size, this.color);
-            drawCircle(obj.x+dist, obj.y, this.size, this.color);
-            drawCircle(obj.x, obj.y+dist, this.size, this.color);
-            drawCircle(obj.x+dist, obj.y+dist, this.size, this.color);
-            
-            this.x = obj.x - this.size;
-            this.y = obj.y - this.size;
-            this.width = this.size*2+dist
-            this.height = this.width;
-            // show hitbox
-            // ctx.strokeStyle = "white";
-            // ctx.rect(this.x,this.y,this.width,this.height);
-            // ctx.stroke();
-        } else {
-            this.done = true;
-        }
-    }
-}
-
-var Character = function() {
-    this.width = CELLSIZE;
-    this.height = CELLSIZE;
-    this.baseColor = rgba(255,255,255,0.7);
-    this.colors = ["#ff0000", "orange", "yellow"];
-    this.accel = CELLSIZE/10;
-    this.drag = 0.95;
-    this.timeToFire = 0;
-    this.reloadFire = 10; // constant number of frames to remove
-    this.reloadBomb = 30; 
-    this.timeToBomb = this.reloadBomb;
-    this.vx = 0;
-    this.vy = 0;
-    this.ax = 0;
-    this.ay = 0;
-    this.currExplosion = undefined;
-    this.reset = () => {
-        this.x = (canvas.width - this.width) / 2;
-        this.y = (canvas.height - this.height) / 2;
-        this.health = this.colors.length
-    }
-    this.reset();
-    this.keepInside = () => {
-        if (this.x < 0){
-            this.x = 0;
-            this.vx = 0;
-            this.ax = 0;
-        } else if (this.x + this.width > canvas.width) {
-            this.x = canvas.width - this.width;
-            this.vx = 0;
-            this.ax = 0;
-        } else if (this.y < 0) { 
-            this.y = 0;
-            this.vy = 0;
-            this.ay = 0;
-        } else if (this.y + this.height > canvas.height) {
-            this.y = canvas.height - this.height;
-            this.vy = 0;
-            this.ay = 0;
-        }
-    }
-    this.fire = () => {
-        if (this.timeToFire <= 0){
-            var b = new Bullet(this.centerX, this.centerY, mouse.x, mouse.y);
-            this.timeToFire = this.reloadFire;
-        }
-    }
-    this.takeDamage = () => {
-        this.health -= 1;
-        this.color = this.colors[this.health - 1];
-        this.timeToBomb = 0;
-        this.bomb();
-    }
-    this.bomb = () => {
-        if (this.timeToBomb <= 0){
-            let speedBonus = (max(abs(this.vx),abs(this.vy)))/CELLSIZE
-            let bombSize = CELLSIZE*8*speedBonus;
-            let bombColor = changeColorAlpha(this.color, 0.4)
-            this.currExplosion = new Explosion(this, this.width, bombSize, bombColor);
-            
-            this.timeToBomb = this.reloadBomb;
-        }
-    }
-    this.update = () => {
-        this.color = this.colors[this.health-1]
-        this.vx += this.ax;
-        this.vy += this.ay;
-        this.x += this.vx;
-        this.y += this.vy;
-        this.centerX = this.x + this.width/2;
-        this.centerY = this.y + this.height/2;
-        this.keepInside();
-        
-        // draw explosions
-        if (this.currExplosion != undefined) {
-            this.currExplosion.update();
-            if (this.currExplosion.done == true){
-                this.currExplosion = undefined;
-            }
-        }    
-        
-        // draw halo (shows how ready the bomb is)
-        let halo = CELLSIZE*1.5;
-        let currHaloSize = halo * ( this.timeToBomb/this.reloadBomb)
-        drawCircle(this.centerX, this.centerY, currHaloSize, rgba(255,255,255,0.6));
-        
-        ctx.fillStyle = this.baseColor;
-        if (this.timeToBomb <= 0) {
-            ctx.fillStyle = this.color;
-        }
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-        this.vx *= this.drag;
-        this.vy *= this.drag;
-        // DEPRECATED FIRE COMMAND
-        // if (mouse.clicked) {
-            // this.fire();
-        // }
-        // if (this.timeToFire > 0) this.timeToFire--;
-        if (this.timeToBomb > 0) this.timeToBomb--;
-    }
-}
-
 function keyToggle(kc, result){    
     if (kc == W_KEY || kc == UP_KEY) UP = result;
     if (kc == S_KEY || kc == DOWN_KEY) DOWN = result;
@@ -331,23 +116,6 @@ function keyUpHandler(ev){
     keyToggle(kc, false)
     if (!UP && !DOWN)     char.ay = 0;
     if (!LEFT && !RIGHT)  char.ax = 0;
-}
-
-mouseDownHandler = (ev) => { 
-    mouse.clicked = true;
-    mouse.x = ev.x;
-    mouse.y = ev.y;
-}
-
-mouseMoveHandler = (ev) => {
-    mouse.x = ev.x;
-    mouse.y = ev.y;
-}
-
-mouseUpHandler = (ev) => { 
-    mouse.clicked = false;
-    mouse.x = undefined;
-    mouse.y = undefined;
 }
 
 function fillScreen(background){
